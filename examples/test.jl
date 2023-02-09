9e18b85430288236339f6dd3ba1f97ff16c5ee34
Adrian Maldonado <dmaldona@hawk.iit.edu>
2023-02-09 16:21:24 -0600

    verified gradient

--- examples/test.jl
+++ examples/test.jl
@@ -1,6 +1,7 @@
 import CLSDyn
 using DifferentialEquations
 using Plots
+using Calculus
 
 x0, ps = CLSDyn.load_matpower("examples/case9.m")
 f!(du,u,p,t) = CLSDyn.classic_resfun!(du, u, ps)
@@ -21,7 +22,7 @@ function dg(out, u, p, t)
 end
 
 using SciMLSensitivity
-res = adjoint_sensitivities(
+du0, dp = adjoint_sensitivities(
     sol,
     Vern9(),
     dgdu_continuous = dg,
@@ -29,3 +30,16 @@ res = adjoint_sensitivities(
     abstol = 1e-8,
     reltol = 1e-8
 )
+
+# verify gradient w.r.t initial conditions
+using QuadGK
+function objective(x)
+    temp_prob = ODEProblem(f!,x,tspan)
+    sol = solve(temp_prob,Vern9())
+    res, err = quadgk((t)-> (sol(t)[1]^2)/2,0.0,5.0,atol=1e-14,rtol=1e-10)
+    res
+end
+
+res2 = Calculus.gradient(objective,u0)
+println(du0)
+println(res2)
