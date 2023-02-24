1d9a7b774409f96f656f8bed751b1e2b5032098a
Adrian Maldonado <dmaldona@hawk.iit.edu>
2023-02-24 14:28:38 -0600

    parametric TLM

--- examples/sensitivities_develop.jl
+++ examples/sensitivities_develop.jl
@@ -27,17 +27,19 @@ function lorenz_fp!(df, dp, x, p, t)
     ρ = p[2]
     β = p[3]
 
-    df[1] = (x[2] - x[1])*dp[1]
-    df[2] = x[1]*dp[2]
-    df[3] = -x[3]*dp[3]
+    df[1] += (x[2] - x[1])*dp[1]
+    df[2] += x[1]*dp[2]
+    df[3] += -x[3]*dp[3]
 end
 
-function rk4_step!(xnew::AbstractArray,
-                   f::Function,
-                   xold::AbstractArray,
-                   p::AbstractArray,
-                   t::Float64,
-                   dt::Float64)
+function rk4_step!(
+    xnew::AbstractArray,
+    f::Function,
+    xold::AbstractArray,
+    p::AbstractArray,
+    t::Float64,
+    dt::Float64
+)
     # Note: this is a bit inefficient because I allocate the k's each time
     # i take a step. the best way might be to pass a data structure
     # where the k's are already allocated.
@@ -77,9 +79,18 @@ Computes the Tangent Linear Model. If pidx = 0, computes the TLM with respect
 to given perturbation direction δx0. Otherwise, computes TLM with respect to
 perturbation of parameter pidx.
 
+Important: fp must _add_ values to the vector.
+
 """
-function tlm(fx::Function, fp::Function, dx0::Vector, traj::AbstractArray,
-             tvec::AbstractArray, p::Vector, pidx::Int=0)
+function tlm(
+    fx::Function,
+    fp::Function,
+    dx0::Vector,
+    traj::AbstractArray,
+    tvec::AbstractArray,
+    p::AbstractArray;
+    dp::Union{AbstractArray, Nothing}=nothing,
+)
 
     nsteps = size(tvec, 1)
     tlm_traj = similar(traj)
@@ -89,14 +100,16 @@ function tlm(fx::Function, fp::Function, dx0::Vector, traj::AbstractArray,
     #    f(dx, x, p, t)
     # but instead of changing the parameter, we change "x" at each step, that is given
     # by the solution of the forward model.
-    if pidx == 0
-        tlm_rhs!(df, dx, x, t) = fx(df, dx, x, p, t)
+
+    function tlm_rhs!(df, dx, x, t)
+        fx(df, dx, x, p, t)
+        if dp != nothing
+            fp(df, dp, x, p, t)
+        end
     end
 
-    println(size(tlm_traj, 1))
-    println(size(tlm_traj, 2))
     nsteps = size(tvec, 1)
-    for i=1:(nsteps - 1)
+    for i = 1:(nsteps - 1)
         t = tvec[i]
         x = @view(traj[:, i])
         dt = tvec[i + 1] - tvec[i]
@@ -107,8 +120,8 @@ function tlm(fx::Function, fp::Function, dx0::Vector, traj::AbstractArray,
 end
 
 # TEST PROBLEM
-tspan=(0.0, 1.0)
-pvec = [10.0, 2.0, 8.0/3.0]
+tspan=(0.0, 0.5)
+pvec = [10.0, 5.0, 8.0/3.0]
 x0 = [1.0, 1.0, 1.0]
 nsteps = 500
 
@@ -118,18 +131,30 @@ traj, tvec = rk4(lorenz_rhs!, x0, pvec, tspan, nsteps)
 ps = plot(tvec, traj[idx_var,:])
 
 # COMPUTE TLM with given dx perturbation
-
+println("Testing TLM")
 # 1.- integrate perturbed system and obtain sensitivities
 #     via finite differences at t_end
-ϵ = 1e-7
+ϵ = 1e-5
 x0eps = [1.0 + ϵ, 1.0, 1.0]
 traj2, tvec2 = rk4(lorenz_rhs!, x0eps, pvec, tspan, nsteps)
-
 println((traj2[:,end]-traj[:,end])/ϵ)
 
+# 2.- integrate TLM
 dx0 = [1.0, 0.0, 0.0]
 tlm_traj  = tlm(lorenz_fx!, lorenz_fp!, dx0, traj, tvec, pvec)
 
 println(tlm_traj[:,end])
 
+# COMPUTE TLM with given dx perturbation
+println("Testing parametric TLM")
 
+# 1.- integrate perturbed system and compute finite differences
+peps = [pvec[1], pvec[2] + ϵ, pvec[3]]
+traj3, tvec3 = rk4(lorenz_rhs!, x0, peps, tspan, nsteps)
+println((traj3[:,end]-traj[:,end])/ϵ)
+
+# 2.- integrate TLM with parameters
+dx0 = [0.0, 0.0, 0.0]
+dp = [0.0, 1.0, 0.0]
+tlm_traj2  = tlm(lorenz_fx!, lorenz_fp!, dx0, traj, tvec, pvec, dp=dp)
+println(tlm_traj2[:,end])
